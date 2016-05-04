80978ebadc3fbfbbe3a65d4039bb5525404dbcc9
Carlos Manzo <carlos.manzo@ennova.it>
2016-05-04 15:08:39 +0200

    Fix url path

--- protected/yiic.php
+++ protected/yiic.php
@@ -5,4 +5,4 @@ $config=dirname(__FILE__).'/config/main.php';
 
 $config = require($config);
 
-require_once('/opt/frameworks/php/yii/framework/yiic.php');
+require_once('../../yii/framework/yiic.php');
